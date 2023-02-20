1f4b1841197d12db42dc71d95ce0a99656cb0d83
VideoDanilka <118012042+VideoDanilka@users.noreply.github.com>
2023-02-20 21:30:29 +0300

    Create app.py

---
+++ WEB_Введение_во_flask_Обработка_HTML-форм/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+import ws
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    with open('design.html', 'r', encoding='utf-8') as html_stream:
+        html = html_stream.read()
+    weather = ws.get_weather()
+    for replace in weather:
+        html = html.replace(f'{{{{ {replace} }}}}', str(weather[replace]))
+    return html
+
+
+if __name__ == '__main__':
+    app.run(port=8000, host='127.0.0.1', debug=True)
