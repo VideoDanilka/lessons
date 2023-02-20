332dc6554e11302803c13496e6249148cca7f9d4
VideoDanilka <118012042+VideoDanilka@users.noreply.github.com>
2023-02-20 19:08:17 +0300

    Create main.py

---
+++ WEB_Решение_задач_на_API_Яндекс.Карт/main.py
@@ -0,0 +1,30 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+import ws
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+        uic.loadUi('design.ui', self)
+
+        self.onSearch()
+
+
+    def onSearch(self):
+        weather = ws.get_weather(self.query.text())
+        if weather:
+            icon = QPixmap()
+            icon.loadFromData(weather['icon'])
+            self.icon.setPixmap(icon)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    wnd = Window()
+    wnd.show()
+    sys.exit(app.exec())
