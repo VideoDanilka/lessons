614a8419de5459629210ad20d17f6c5b6d309a79
VideoDanilka <118012042+VideoDanilka@users.noreply.github.com>
2023-02-20 21:30:07 +0300

    Create ws.py

---
+++ WEB_Введение_во_flask_Обработка_HTML-форм/ws.py
@@ -0,0 +1,23 @@
+import os
+from requests import request
+
+API_KEY = os.environ.get('API_KEY')
+
+
+def get_weather(query=''):
+    if not query:
+        query = 'fetch:ip'
+    response = request(
+        method='GET',
+        url='https://api.weatherstack.com/current',
+        params={
+            'access_key': API_KEY,
+            'query': query
+        }
+    )
+    if response.status_code == 200:
+        return {
+            'icon': response.json()['current']['weather_icons'][0],
+            'temperature': response.json()['current']['temperature'],
+            'wind_speed': response.json()['current']['wind_speed']
+        }
